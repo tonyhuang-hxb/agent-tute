f4c6a77c2b8fff7477126783caea50cb7a0c8ed6
tonyhuang-hxb <dev@tonyhuang.id.au>
2025-12-21 23:00:35 +0800

    feat: add parsing and prompt management

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,16 +12,35 @@ def main():
         raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
     
     client = genai.Client(api_key=api_key)
-    model = "gemini-2.5-flash"
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    print("Prompt:", prompt)
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("model", type=str, help="Model to use", default="gemini-2.5-flash", nargs='?')
+    args = parser.parse_args()
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     response = client.models.generate_content(
-        model=model, contents=prompt
+        model=args.model, contents=messages
     )
+    
+    metadata = response.usage_metadata
+    
+    if metadata is None:
+        raise RuntimeError("Usage metadata is missing in the response. API request may have failed")
+    
+    prompt_token_count = metadata.prompt_token_count
+    candidates_token_count = metadata.candidates_token_count
+    
+    print("="*10)
+    print("Prompt:", args.user_prompt)
+    print()
     print("Response:\n" + response.text)
-
-
+    print("="*10)
+    print("Metadata:", metadata)
+    print("Prompt tokens:", prompt_token_count)
+    print("Response tokens:", candidates_token_count)
+    
 
 if __name__ == "__main__":
     main()
